9e34513724b8a382d4326f0ce00a258314c76f8e
LeHoang <lehoang999113@gmail.com>
2023-06-21 10:46:15 +0700

    patch product

--- routers/ProductRouter.js
+++ routers/ProductRouter.js
@@ -4,12 +4,13 @@ const { ProductMiddleware } = require('../middlewares/ProductMiddleware');
 const ProductRouter = express.Router();
 ProductRouter.use(ProductMiddleware);
 
-const { createProduct, getAllProduct, getProductById, updateProductById ,deleteProductById} = require('../controllers/ProductController');
+const { createProduct, getAllProduct, getProductById, updateProductById, deleteProductById } = require('../controllers/ProductController');
 
 ProductRouter.post('/products', createProduct);
 ProductRouter.get('/products', getAllProduct);
 ProductRouter.get('/products/:id', getProductById);
 ProductRouter.put('/products/:id', updateProductById);
+ProductRouter.patch('/products/:id', updateProductById);
 ProductRouter.delete('/products/:id', deleteProductById);
 
 module.exports = ProductRouter;
